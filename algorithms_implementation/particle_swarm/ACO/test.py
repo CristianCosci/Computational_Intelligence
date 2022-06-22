c0f7d9e4f8e1127fcb2e9112a10b7a680195c341
CristianCosci <cristiancosci@gmail.com>
2022-06-22 21:43:27 +0200

    corretto ACO

--- algorithms_implementation/particle_swarm/ACO/test.py
+++ algorithms_implementation/particle_swarm/ACO/test.py
@@ -1,9 +1,15 @@
 from ACO import *
+import os
+CURR_DIR = os.path.dirname(os.path.realpath(__file__))
+print(CURR_DIR)
+print (os.getcwd())
 
-pr = Tsp_problem.read_file("berlin52.tsp")
+pr = Tsp_problem.read_file("algorithms_implementation/particle_swarm/ACO/berlin52.tsp")
 
 a = ACO_for_TSP(pr)
 
 print(a.alpha)
 
 a.run(500, 121)
+
+
